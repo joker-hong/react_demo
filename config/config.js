37bfaadb92e417a0cfafa00c0360645536f5e855
hjh <373599271@qq.com>
2020-04-14 18:25:32 +0800

    v1.0

--- config/config.js
+++ config/config.js
@@ -1,14 +1,27 @@
 export default {
+    dva: {},
+    antd: {},
     singular: true,
-
-    plugins: [
-        ['umi-plugin-react', {
-            
-        }]
-    ],
-
     routes: [{
         path: '/',
-        component: './HelloWorld'
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: './HelloWorld'
+            },
+            {
+                path: '/helloworld',
+                component: './HelloWorld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
+            }
+        ]
     }]
 }
