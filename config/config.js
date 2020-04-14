3be8f186ac64e032488f445a46cbca67425c8cd0
hjh <373599271@qq.com>
2020-04-14 11:45:43 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export default {
+    singular: true,
+
+    plugins: [
+        ['umi-plugin-react', {
+            
+        }]
+    ],
+
+    routes: [{
+        path: '/',
+        component: './HelloWorld'
+    }]
+}
